4b0dd421acb530d8b423f4d8d72dda8a3a79fdea
unknown <juliosilva0558@digitalcollege.internal>
2024-12-20 11:06:07 -0300

    Criando repositorio novo projeto

---
+++ aula04/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Teste from './components/Teste.jsx'
+
+function App() {
+  return (
+    <>
+      <Teste />
+    </>
+  )
+}
+
+export default App
---
+++ aula04/src/components/Teste.jsx
@@ -0,0 +1,8 @@
+const Teste = () => {
+    return (
+        <>
+            <h1>a</h1>
+        </>
+    )
+}
+export default Teste
