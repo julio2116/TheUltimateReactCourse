edacc05feafd46c11fbbe29b2d9e6ece6442d07b
jb2116 <juliobatista2116@gmail.com>
2024-12-25 23:03:50 -0300

    iniciando sessão 12

--- aula04-Projeto2/src/App.jsx
+++ aula04-Projeto2/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import TextExpender from "./components/TextExpender.jsx";
 
 function App() {
   return (
--- aula04-Projeto2/src/components/TextExpender.jsx
+++ aula04-Projeto2/src/components/TextExpender.jsx
@@ -1,7 +1,27 @@
-const TextExpender = () => {
+import { Children, useState } from "react"
+
+const TextExpender = ({children,
+    collapseNumWords=10, expandButtonText='Show more',
+    collapseButtonText='Show less', buttonColor='blue',
+    buttonInline, className}) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const words = children.split(" ").splice(0, collapseNumWords).join(' ');
+
+    let showWords = isOpen ? children : words+' ...'
+
+    function handleShowWords(){
+        setIsOpen(isOpen=>!isOpen);
+    }
+    
     return(
         <>
-        
+        <div className={className}>
+        {showWords}
+        <span style={{color:buttonColor}} onClick={handleShowWords}>
+            {!isOpen ?
+            expandButtonText : collapseButtonText}
+        </span>
+        </div>
         </>
     )
 }
