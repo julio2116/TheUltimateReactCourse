adeb29a14bb1847598a5bc1d714d2fc9cdd55254
jb2116 <juliobatista2116@gmail.com>
2025-01-08 17:21:30 -0300

    Finalizando aula06 sessão 17

--- aula06/src/components/Map.jsx
+++ aula06/src/components/Map.jsx
@@ -1,9 +1,15 @@
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 
 function Map(){
+    const navigate = useNavigate()
+    const [searchParams, setSearchParams] = useSearchParams();
+    const lat = searchParams.get('lat')
+    const lng = searchParams.get('lng')
+
     return (
         <>
-        <div className={styles.mapContainer}>a</div>
+        <div className={styles.mapContainer} onClick={()=>{navigate('form')}}>{lat}</div>
         </>
     )
 }
