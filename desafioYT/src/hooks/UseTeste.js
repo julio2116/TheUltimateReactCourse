6aba324b18e131050a7d802359af8cdefc9f74ef
unknown <juliosilva0558@digitalcollege.internal>
2025-01-29 09:43:44 -0300

    Consertando renderizações desnecessárias no useTeste

--- desafioYT/src/hooks/UseTeste.js
+++ desafioYT/src/hooks/UseTeste.js
@@ -6,9 +6,7 @@ function useTeste() {
   const termSearched = term.get("search");
   const [searchResult, setSearchResult] = useState([]);
   const [views, setViews] = useState([]);
-  const [viewsIds, setViewsIds] = useState("");
   const [channels, setChannels] = useState([]);
-  const [channelsIds, setChannelsIds] = useState("");
   // const key = "AIzaSyCB0gEZJ25Whe87CQvgsKGlMT6_pS8Wpdo";
   // const key = "AIzaSyDnrpgoUVD1uxJ8ijOdxhefHUb9ChiG9Bk";
   const key = "AIzaSyCvJM7ZW8I2K0JEnOO76qa9w0DUyrg8VrA";
@@ -21,22 +19,28 @@ function useTeste() {
         );
         const data = await res.json();
         setSearchResult(data);
-        const resultViwes = data?.items?.map((item) => item.id.videoId).join();
-        const resultChannels = data.items
-          .map((item) => item.snippet.channelId)
-          .join();
-        setViewsIds(() => resultViwes);
-        setChannelsIds(() => resultChannels);
       }
       fetchSearchResult();
+    },
+    [termSearched]
+  );
+  useEffect(
+    function () {
       async function fetchViews() {
+        const viewsIds = searchResult?.items?.map((item) => item.id.videoId).join();
         const res = await fetch(
           `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${viewsIds}&key=${key}`
-        );
-        const data = await res.json();
-        setViews(() => data);
-      }
-      fetchViews();
+          );
+          const data = await res.json();
+          setViews(() => data);
+        }
+        fetchViews();
+    },
+    [searchResult]
+  );
+  useEffect(
+    function () {
+      const channelsIds = searchResult?.items?.map((item) => item.snippet.channelId).join();
       async function fetchChannels() {
         const res = await fetch(
           `https://www.googleapis.com/youtube/v3/channels?part=statistics,contentDetails,snippet&id=${channelsIds}&key=${key}`
@@ -46,13 +50,11 @@ function useTeste() {
       }
       fetchChannels();
     },
-    [viewsIds, channelsIds, termSearched]
+    [searchResult]
   );
 
   function joinObjectsVideos() {
-
-
-    const teste1 = searchResult?.items?.map((item) => ({
+    const allSearchResultsObjects = searchResult?.items?.map((item) => ({
       id: item.etag,
       kind: item.id.kind,
       channelId: item.snippet.channelId,
@@ -62,17 +64,17 @@ function useTeste() {
       thumb: item.snippet.thumbnails.high.url,
       published: item.snippet.publishedAt,
     }));
-    const teste2 = channels?.items?.map((item) => ({
+    const allChannelsObject = channels?.items?.map((item) => ({
       channelId: item.id,
       icon: item.snippet.thumbnails.high.url,
       channelTitle: item.snippet.title,
     }));
-    const teste3 = views?.items?.map((item) => ({
+    const allViewsObject = views?.items?.map((item) => ({
       videoId: item.id,
       views: item.statistics.viewCount,
     }));
-    const joinVideoChannel = teste1?.map((item1) =>
-      teste2
+    const joinVideoChannel = allSearchResultsObjects?.map((item1) =>
+      allChannelsObject
         ?.filter((item2) => item2.channelId === item1.channelId)
         .map((item2) => Object.assign({}, item1, item2))
     );
@@ -81,7 +83,7 @@ function useTeste() {
       .filter(Boolean);
 
     const joinVideoChannelViews = newJoinVideoChannel?.map((item1) =>
-      teste3
+      allViewsObject
         ?.filter((item3) => item3.videoId === item1.videoId)
         .map((item3) => Object.assign({}, item1, item3))
     );
