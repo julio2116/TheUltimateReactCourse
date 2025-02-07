dd1b66015f93a7173e8a4ceb56f880e8406b15f6
unknown <juliosilva0558@digitalcollege.internal>
2025-02-07 18:25:17 -0300

    Finalizando projeto fast-react-pizza

---
+++ fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router'
+import SearchOrder from '../features/order/SearchOrder'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <Link to='/'>Fast React Pizza Co.</Link>
+                <SearchOrder />
+                <p>Jonas</p>
+            </header>
+        </>
+    )
+}
+export default Header
