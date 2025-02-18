4a9cf9676599854a8047d279f42bdcdb58b264c3
unknown <juliosilva0558@digitalcollege.internal>
2025-02-18 14:53:19 -0300

    removing yt directory

--- desafioYT/src/components/VideoPage/RecomendedVideosList.jsx
+++
@@ -1,8 +0,0 @@
-const RecomendedVideosList = () => {
-  return (
-    <>
-      <h1 style={{ minWidth: "402px", marginTop: "100px" }}>SideContent</h1>
-    </>
-  );
-};
-export default RecomendedVideosList;
